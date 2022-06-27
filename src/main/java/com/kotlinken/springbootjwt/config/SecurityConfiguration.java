3f6a0db59c6ed2b55bf2143e883793ff402727c4
kotlin <kotlinken@gmail.com>
2022-06-27 17:16:09 +0900

    Security Configuration clean up

--- src/main/java/com/kotlinken/springbootjwt/config/SecurityConfiguration.java
+++ src/main/java/com/kotlinken/springbootjwt/config/SecurityConfiguration.java
@@ -8,19 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static com.kotlinken.springbootjwt.config.MyCustomDsl.customDsl;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-
-
     private final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
     
     private final JwtProperties jwtProperties;
@@ -28,10 +23,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         final AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-
-//            http.apply(new JwtHttpConfiguration(jwtProperties));
         http.addFilter(new JwtAuthenticationFilter(authenticationManager, jwtProperties));
-
         return http.build();
     }
 }
